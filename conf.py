b2fd9bd73ea8ee400c4d25fb4856652074630b4d
ciro spataro <cirospat@gmail.com>
2018-06-06 11:04:09 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -36,5 +36,6 @@ def setup(app):
   Because the pdflatex raises exception when generate Latex documents with CKJ characters.
 """
 #latex_documents = []
+
 latex_logo = "static/ibm-smarter.png"
 html_logo = "static/ibm-smarter.png"
